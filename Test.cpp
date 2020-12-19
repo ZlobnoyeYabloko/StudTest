fc341af480723def7e0d0a1dc89e3ed2306652cd
Nikita <zurtis228@gmail.com>
2020-12-19 14:54:57 +0200

    add "cin>>b"

--- Test.cpp
+++ Test.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 int main()
 {
-	int c;
+	int c,b;
 	cin >> c;
+	cout<<"Vedite c: "<<endl;
+	cin >> b;
+	cout<<"Vedite b: "<<endl;
 	if (c > 1)
 	{
 		cout << "Hello World" << endl;
