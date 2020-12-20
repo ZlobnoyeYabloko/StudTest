e68ec1b5e4d855cc86670b34813f30327f6995c0
Nikita <zurtis228@gmail.com>
2020-12-20 14:35:29 +0200

    add Test№3

---
+++ Test №3.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+
+using namespace std;
+int main()
+{
+	int c, b;
+	cin >> b;
+	cout << "Vedite B: " << endl;
+	cin >> c;
+	cout << "Vedite C: " << endl;
+	cout << "Hello World #2" << endl;
+}
