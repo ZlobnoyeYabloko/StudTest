80f027a2cd3c3f3a96f7649abefa58d9f084f2ef
Nikita <zurtis228@gmail.com>
2020-12-20 17:20:37 +0200

    add n,m

--- Test Kod.cpp
+++ Test Kod.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-	int a, b, c, d;
+	int a, b, c, d,n,r;
 	cout << "Vedite a,b,c" << endl;
 	cout << "Vedite a: ";
 	cin >> a;
@@ -21,6 +21,11 @@ int main()
 	c1 = c / 100;
 	d1 = d / 100;
 
+	n = a + b;
+
+	cout << "Result: " << n << endl;
+
+	
 	if (d1>c1>b1>a1) {
 		cout << "true" << endl;
 	}
