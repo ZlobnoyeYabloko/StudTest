266df86b469f1549bbca36956e02a00318db4df1
Nikita <zurtis228@gmail.com>
2020-12-20 14:29:48 +0200

    new TestVeryMegaTest

--- Test.cpp
+++ Test.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 int main()
 {
-	int c,b;
+	int c,b,n;
 	cin >> c;
 	cout<<"Vedite c: "<<endl;
 	cin >> b;
 	cout<<"Vedite b: "<<endl;
+	cin >> n;
+	cout << "Vedite n: " << endl;
 	if (c > 1)
 	{
 		cout << "Hello World" << endl;
---
+++ TestVeryMegaTest.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+int main()
+{
+	int c, b;
+	cin >> b;
+
+	cout << "Vedite B: " << endl;
+
+	cout << "Hello" << endl;
+
+}
